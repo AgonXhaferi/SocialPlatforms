605a179c2e50d920a3d58521e4c094fb757f7f4c
Agon Xhaferi <agonxhaferi99@gmail.com>
2024-01-15 20:57:33 +0100

    Added project base structure for HDDD architechture.

---
+++ src/libs/utils/dotenv.ts
@@ -0,0 +1,10 @@
+// import { config } from 'dotenv';
+// import * as path from 'path';
+//
+// // Initializing dotenv
+// const envPath: string = path.resolve(
+//   __dirname,
+//   process.env.NODE_ENV === 'test' ? '../../../.env.test' : '../../../../.env',
+// );
+// config({ path: envPath });
+//TODO: I don't think this is necessary for the latest nestjs
