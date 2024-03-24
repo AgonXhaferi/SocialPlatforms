b0d3a18243342d0056dc286918710b73b467982f
Agon Xhaferi <agonxhaferi99@gmail.com>
2024-03-24 13:20:53 +0100

    Removed uneccessary comment

--- src/modules/user/application/ports/user.repository.port.ts
+++ src/modules/user/application/ports/user.repository.port.ts
@@ -1,6 +1,5 @@
 import { AggregateID, RepositoryPort } from '@libs/ddd';
 import { UserEntity } from '@modules/user/domain/entities/user.entity';
 
-//This should not be the persistence/database entity, this should be the aggregate entity.
 export interface UserRepositoryPort
   extends RepositoryPort<UserEntity, AggregateID> {}
